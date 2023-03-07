ac455965d670a3bf8e2a9a63636f35fb647467af
Giovanni Caramello <100931407+iamGiovanniCaramelloGrego@users.noreply.github.com>
2023-03-07 11:07:25 -0300

    07/03

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", navToggle);
 
- const navToggler = document.querySelector(".nav-toggler");
- navToggler.addEventListener("click", navToggle);
+function navToggle() {
+   navToggler.classList.toggle("active");
+   const nav = document.querySelector(".nav");
+   nav.classList.toggle("open");
+   if(nav.classList.contains("open")){
+      nav.style.maxHeight = nav.scrollHeight + "px";
+   }
+   else{
+      nav.removeAttribute("style");
+   }
+} 
 
- function navToggle() {
-    navToggler.classList.toggle("active");
-    const nav = document.querySelector(".nav");
-    nav.classList.toggle("open");
-    if(nav.classList.contains("open")){
-    	nav.style.maxHeight = nav.scrollHeight + "px";
-    }
-    else{
-    	nav.removeAttribute("style");
-    }
- } 
+function troca0(){
+   window.location.href = "index.html"
+
+}
+
+function troca(){
+
+   window.location.href = "DS1.HTML"
+}
+
+function troca1(){
+   window.location.href = "DS3.HTML"
+}
+
+function troca2(){
+   window.location.href = "ER.HTML"
+}
+
+function troca3(){
+   window.location.href = "SEK.HTML"
+}
